5e8a57e084d8860cd5f06c67c3df0d17f7b57062
limsh9836 <lim.s.h9836@gmail.com>
2020-12-22 23:11:47 +0800

    Added DL Model

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./assets'));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true});
@@ -20,10 +21,13 @@ connection.once('open',()=>{
 // const medicalRouter = require('./routes/medical');
 // const patientRouter = require('./routes/patient');
 const userRoute = require('./routes/users');
+// const testRoute = require('./routes/model');
 
 // app.use('/medical',medicalRouter);
 // app.use('/patient',patientRouter);
 app.use('/users', userRoute);
+
+// app.use('model/', testRoute);``
 
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`);
